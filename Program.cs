f268a805ceba9ac7d0ce5eb85da32f90d144d4b4
rodilconcha <159867279+rodilconcha@users.noreply.github.com>
2024-07-07 23:04:02 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+interface IQuittable
+{
+    void Quit();
+}
+class Employee : IQuittable
+{
+    public string Name { get; set; }
+
+    // Implement the Quit() method
+    public void Quit()
+    {
+        Console.WriteLine($"{Name} has quit the job.");
+    }
+}
+static void Main(string[] args)
+{
+    Employee employee = new Employee { Name = "John" };
+    IQuittable quittableEmployee = employee;
+
+    // Call Quit() using polymorphism
+    quittableEmployee.Quit();
+
+    // Output: "John has quit the job."
+}
